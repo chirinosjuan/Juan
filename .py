d9c54f8535bb519f81caac69564039a2a4894745
chirinosjuan <100810104+chirinosjuan@users.noreply.github.com>
2022-03-16 01:23:55 +0100

    Add files via upload

--- .py
+++ .py
@@ -1,4 +1,4 @@
 from ast import Name
 from unicodedata import name
-name =["Juan Jose Chirinos Hernandez"] 
-print ("Hola mundo", name) 
+Name =("Juan Jose Chirinos Hernandez") 
+print ("Hola mundo", Name) 
